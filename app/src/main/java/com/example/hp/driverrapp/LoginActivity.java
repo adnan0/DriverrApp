c55348b39ee9302d6d2fe66dc4d2a80941d7248e
adnan0 <adnan42096@gmail.com>
2018-06-15 02:28:27 +0500

    second update

--- app/src/main/java/com/example/hp/driverrapp/LoginActivity.java
+++ app/src/main/java/com/example/hp/driverrapp/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.example.hp.driverrapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,31 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        /**** Button btn = (Button) findViewById(R.id.UpBtn);
+         btn.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View view) {
+        openFragment();
+        }
+        });
+         */
+        View.OnClickListener listener = new View.OnClickListener() {
+            public void onClick(View view) {
+                Fragment fragment = null;
+                if (view == findViewById(R.id.UpBtn)) {
+                    fragment = new SignUpFrag();
+                }
+
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.SFrag, fragment);
+                transaction.commit();
+            }
+        };
+
+
     }
 }
+
+
